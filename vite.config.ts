2699076f0249aba94ae0b329a7794d373953ba38
amoshydra <8733840+amoshydra@users.noreply.github.com>
2024-12-20 04:50:21 +0800

    chore: configure continuous deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const BASE_URL = process.env.BASE_URL || "";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `${BASE_URL}/`,
   plugins: [react(), tailwindcss()],
 });
