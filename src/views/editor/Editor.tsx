01909f733c1f0136e794e358f9166adaf102d375
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-01-19 23:39:05 +0800

    feat: allow switching of editor via query param

--- src/views/editor/Editor.tsx
+++ src/views/editor/Editor.tsx
@@ -2,11 +2,20 @@ import { lazy, Suspense } from 'react';
 import { withCn } from "../../utils/tailwind";
 import { EditorProps } from './interface';
 
-const LaziedCodeMirror = lazy(async () => ({ default: (await import("./Editor.CodeMirror")).CodeMirrorEditor }));
-const LaziedMonaco = lazy(async () => ({ default: (await import("./Editor.Monaco")).MonacoEditor }));
-const PlaceholderEditor = (p: { value: string }) => <textarea className='h-full w-full' readOnly value={"// Loading editor...\n\n" + p.value} />
+const editorPreference = ((): "monaco" | "codemirror" => {
+  const params = new URLSearchParams(location.search);
+  const editorValue = params.get("editor") ?? "monaco";
+  return editorValue === "codemirror" ? "codemirror" : "monaco";
+})();
 
-const EditorToRender = true ? LaziedMonaco : LaziedCodeMirror;
+const getPreferredEditor = async () => {
+  if (editorPreference === "monaco") {
+    return (await import("./Editor.Monaco")).MonacoEditor;
+  }
+  return (await import("./Editor.CodeMirror")).CodeMirrorEditor
+}
+// start fetching editor as this module load
+const getPreferredEditorPromise = getPreferredEditor().then(component => ({ default: component }));
 
 export const Editor = ({ value, onValueChange, ...props }: EditorProps) => {
   return (
@@ -14,7 +23,7 @@ export const Editor = ({ value, onValueChange, ...props }: EditorProps) => {
       <Suspense
         fallback={<PlaceholderEditor value={value} />}
       >
-        <EditorToRender
+        <LaziedEditor
           value={value}
           onValueChange={onValueChange}
         />
@@ -22,3 +31,6 @@ export const Editor = ({ value, onValueChange, ...props }: EditorProps) => {
     </div>
   );
 };
+
+const PlaceholderEditor = (p: { value: string }) => <textarea className='h-full w-full' readOnly value={"// Loading editor...\n\n" + p.value} />
+const LaziedEditor = lazy(() => getPreferredEditorPromise);
