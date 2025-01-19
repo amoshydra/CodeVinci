9f1eebda84b8266c9966d5dd7d5fbc159717ab54
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-01-20 00:04:00 +0800

    fix: ignore first call properly, add explanation on the patch
    this allow onValueChange to fire when a real empty string is provided

--- src/views/editor/Editor.Monaco.tsx
+++ src/views/editor/Editor.Monaco.tsx
@@ -1,17 +1,25 @@
 import ReactMonacoEditor, { MonacoEditorProps } from '@uiw/react-monacoeditor';
-import { memo, useRef } from 'react';
+import { memo, useMemo, useRef, useState } from 'react';
 import { EditorProps } from './interface';
 
 
-export const MonacoEditor = memo(({ value, onValueChange }: EditorProps) => {
-  const { current: initialValue } = useRef(value);
+export const MonacoEditor = memo((p: EditorProps) => {
+  // @uiw/react-monacoeditor@3.6.0 will cause cursor to be reset when value is changed
+  // this useRef retained the `value` prop, prevent the underlying model to be reset.
+  //
+  // additionally, ReactMonacoEditor's value prop is not strictly operating in controlled mode.
+  // It can be used as an uncontrolled prop.
+  const { current: initialValue } = useRef(p.value);
+
+  // @uiw/react-monacoeditor@3.6.0 will always call `onChange` with an empty string value `""` on mount
+  //
+  // this patchignore the first call
+  const onValueChange = useIgnoreFirstCall(p.onValueChange);
+
   return (
     <ReactMonacoEditor
       value={initialValue}
-      onChange={(value) => {
-        if (!value) return;
-        onValueChange(value)
-      }}
+      onChange={onValueChange}
       options={options}
       language='javascript'
       height="100%"
@@ -47,3 +55,16 @@ const options: MonacoEditorProps["options"] = {
     arrowSize: 30,
   },
 } as const;
+
+const useIgnoreFirstCall = (onValueChange: (s: string) => void) => {
+  const [hasBeenCalled, setHasBeenCalled] = useState(false);
+
+  return useMemo(() => {
+    if (!hasBeenCalled) {
+      setHasBeenCalled(true);
+      return () => {};
+    }
+
+    return onValueChange;
+  }, [hasBeenCalled, onValueChange])
+}
