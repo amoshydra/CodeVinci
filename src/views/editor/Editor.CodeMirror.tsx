dd0e48dccf5d39a702528f990018a47c999c3ee9
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-01-19 23:37:26 +0800

    fix: patch @uiw/react-monacoeditor allow usage in StrictMode environment
    See: https://github.com/uiwjs/react-monacoeditor/issues/183

--- src/views/editor/Editor.CodeMirror.tsx
+++ src/views/editor/Editor.CodeMirror.tsx
@@ -2,7 +2,7 @@ import { javascript } from '@codemirror/lang-javascript';
 import CodeMirror from '@uiw/react-codemirror';
 import { EditorProps } from './interface';
 
-export const CodeMirrorEditor = ({ value, onValueChange, ...props }: EditorProps) => {
+export const CodeMirrorEditor = ({ value, onValueChange }: EditorProps) => {
   return (
     <CodeMirror
       height="100%"
