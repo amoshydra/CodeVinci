2595885ed508c8c7f002be0b7fea6ad2e4f7e974
amoshydra <8733840+amoshydra@users.noreply.github.com>
2026-02-08 21:30:57 +0800

    style: format spacing

--- src/views/disclaimer/Disclaimer.tsx
+++ src/views/disclaimer/Disclaimer.tsx
@@ -1,7 +1,7 @@
+import { lazy, Suspense } from 'react';
 import { Flex, Stack, styled } from '../../../styled-system/jsx';
 import { AppTopBar } from '../../components/AppTopBar';
 import { BrandLogoButton } from '../../components/BrandLogoButton';
-import { lazy, Suspense } from 'react';
 import { PlaceholderEditor } from '../editor/Editor.Placeholder';
 
 const getCodeMirrorPromise = import('../editor/Editor.CodeMirror')
@@ -16,42 +16,42 @@ export interface DisclaimerProps {
 }
 
 export const Disclaimer = ({ code, onAcceptRun, onAcceptEdit }: DisclaimerProps) => {
-  return (<>
-    <AppTopBar>
-      <BrandLogoButton />
-    </AppTopBar>
-    <Stack p="6" gap="8">
+  return (
+    <>
+      <AppTopBar>
+        <BrandLogoButton />
+      </AppTopBar>
+      <Stack p="6" gap="8">
+        <Alert role="alert">
+          <styled.strong fontWeight="bold">Warning!{" "}</styled.strong>
+          <styled.span display={{ base: 'block', sm: 'inline' }}>
+            Executing code from the internet can be dangerous.
+            Please ensure you understand the risks and have taken necessary precautions.
+          </styled.span>
+        </Alert>
 
-      <Alert role="alert">
-        <styled.strong fontWeight="bold">Warning!{" "}</styled.strong>
-        <styled.span display={{ base: 'block', sm: 'inline' }}>
-          Executing code from the internet can be dangerous.
-          Please ensure you understand the risks and have taken necessary precautions.
-        </styled.span>
-      </Alert>
+        <CodeBlock>
+          <Suspense
+            fallback={
+              <PlaceholderEditor
+                value={code}
+                data-editor="codemirror"
+              />
+            }
+          >
+            <LazyCodeMirror value={code} onValueChange={() => {}} readOnly={true} />
+          </Suspense>
+        </CodeBlock>
 
-      <CodeBlock>
-        <Suspense
-          fallback={
-            <PlaceholderEditor
-              value={code}
-              data-editor="codemirror"
-            />
-          }
-        >
-          <LazyCodeMirror value={code} onValueChange={() => {}} readOnly={true} />
-        </Suspense>
-      </CodeBlock>
-
-      <Flex justify="flex-end" gap="4">
-        <ActionButton visual="secondary" onClick={onAcceptEdit}>
-          Comment before run
-        </ActionButton>
-        <ActionButton visual="primary" onClick={onAcceptRun}>
-          Run code
-        </ActionButton>
-      </Flex>
-    </Stack>
+        <Flex justify="flex-end" gap="4">
+          <ActionButton visual="secondary" onClick={onAcceptEdit}>
+            Comment before run
+          </ActionButton>
+          <ActionButton visual="primary" onClick={onAcceptRun}>
+            Run code
+          </ActionButton>
+        </Flex>
+      </Stack>
     </>
   );
 };
